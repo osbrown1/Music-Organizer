f15dce2ca50ff2bc8bf43e9c347dc75dffb69251
osbrown1 <socknot@live.com>
2023-03-02 16:23:01 -0800

    Incomplete, views

--- MusicOrganizer/Program.cs
+++ MusicOrganizer/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ProjectName
+namespace MusicOrganizer
 {
   class Program
   {
@@ -15,7 +15,7 @@ namespace ProjectName
 
       // app.UseDeveloperExceptionPage();
       app.UseHttpsRedirection();
-
+      app.UseStaticFiles();
       app.UseRouting();
 
       app.MapControllerRoute(
